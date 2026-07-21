5fc273fae4b8f86246ef3f325eb9b4ad29a0d28c
Robert Russo <rrusso@lsu.edu>
2026-07-21 21:29:16 +0000

    from LSUonline style guide

--- classes/hook_callbacks.php
+++ classes/hook_callbacks.php
@@ -43,6 +43,7 @@ class hook_callbacks {
 
         $hook->add_html('<link rel="preconnect" href="https://fonts.googleapis.com">');
         $hook->add_html('<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>');
+        $hook->add_html('<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,400;0,700;1,400;1,700&family=Roboto:ital,wght@0,400;0,700;0,900;1,400;1,700;1,900&display=swap">');
         $hook->add_html('<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Source+Sans+3:ital,wght@0,400;0,600;0,700;0,800;1,400&display=swap">');
     }
 }
